b26dd0c4f356a2370c3736798d01747cd7ad6809
MohammadSami <samishaikh6810@gmail.com>
2021-06-17 23:25:44 +0530

    setting

--- MegaProject/settings.py
+++ MegaProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zc5j*#ltnx6#rxv#b-e@*v09q60@xkc93u2+a=714^eltxn5ny'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.103', '127.0.0.1', 't-kart.herokuapp.com' ]
 
@@ -165,6 +165,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+""" 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+ """
