d255da9d0ba68bf17956fdf250456a69459739a7
MohammadSami <samishaikh6810@gmail.com>
2021-06-18 15:55:31 +0530

    heroku update

--- MegaProject/settings.py
+++ MegaProject/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
